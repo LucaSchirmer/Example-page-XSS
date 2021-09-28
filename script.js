8a054443b0a2713f376a36dba29122a136f3ac5f
LucaSchirmer <66419654+LucaSchirmer@users.noreply.github.com>
2021-09-28 20:42:54 +0200

    launched website

---
+++ script.js
@@ -0,0 +1,11 @@
+
+console.log(loc)
+console.log(loc)
+function load(){
+    document.querySelector(".nameOUTPUT").innerHTML = document.write(
+    document.getElementById("name").value
+    ,document.getElementById("email").value
+    ,document.getElementById("password").value
+    
+    );
+}
